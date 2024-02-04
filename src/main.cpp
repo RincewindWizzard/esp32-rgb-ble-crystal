649c1d5d89ab8e5ce4e2cf05047cc3a494a31170
RincewindWizzard <git@magierdinge.de>
2024-02-04 16:39:38 +0100

    Advertising my name BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define DEVICE_NAME "esp32-rgb-crystal-"
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 
 const int LED = 8;
 
@@ -8,6 +17,35 @@ void setup()
 {
     Serial.begin(460800);
     pinMode(LED, OUTPUT);
+
+    uint32_t chipID = ESP.getEfuseMac();
+
+    // Den Chip-Namen erstellen
+    String device_name = DEVICE_NAME + String(chipID, HEX);
+
+
+    Serial.printf("Starting BLE work as %s...\n", device_name.c_str());
+
+    BLEDevice::init(device_name.c_str());
+    BLEServer* pServer = BLEDevice::createServer();
+    BLEService* pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic* pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID,
+        BLECharacteristic::PROPERTY_READ |
+        BLECharacteristic::PROPERTY_WRITE
+    );
+
+
+    pCharacteristic->setValue("Hello World says Neil");
+    pService->start();
+    // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+    BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+    Serial.println("BLE is advertising.");
 }
 
 void loop()
